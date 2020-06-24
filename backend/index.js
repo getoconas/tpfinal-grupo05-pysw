3de71342e61c2b89a3cfb6a2d69251698813bb82
getoconas <getoconas@gmail.com>
2020-06-24 19:36:04 -0300

    Estructura base del backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+const { mongoose } = require('./database');
+const cors = require('cors');
+
+// Middlewares
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:4200'
+}))
+
+// Cargamos el modulo de direccionamiento de rutas para puntos
+/* routes */
+
+// Setting
+app.set('port', process.env.PORT || 3000);
+
+// Staring the server
+app.listen(app.get('port'), () => {
+  console.log('Server started on port', app.get('port'));
+});
