b0491f0afbce47ff1ec86d1c2ca457c32477bd0e
LiliaSaid <yazminsaid@gmail.com>
2020-06-25 00:22:46 -0300

    Se creó las variables de las clses usuario y noticia

--- frontend/src/app/models/noticia.ts
+++ frontend/src/app/models/noticia.ts
@@ -1,2 +1,15 @@
+import { Usuario } from './usuario';
+
 export class Noticia {
+    _id: string;
+    titulo: string;
+    descripcion: string;
+    fecha: Date;
+    usuario: Usuario;
+    vigente: boolean;
+
+    constructor() {
+    
+    }
+
 }
--- frontend/src/app/models/usuario.ts
+++ frontend/src/app/models/usuario.ts
@@ -1,2 +1,12 @@
 export class Usuario {
+    
+    _id: string;
+    usuario: string;
+    password: string;
+    activo: boolean;
+    perfil: string;
+
+    constructor() {
+    
+    }
 }
