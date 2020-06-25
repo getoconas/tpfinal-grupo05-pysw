218faab71b8019d3c3b130b5ef9f2a9da7a8daea
yesicaMamani <yesica.mamani86@gmail.com>
2020-06-25 03:08:42 -0300

    se completaron las clases servicio y novedad del front, las clases usuario y afiliado del back, tambien se agrego contenido en el componente inicio  a modo de prueba.

--- frontend/src/app/models/novedad.ts
+++ frontend/src/app/models/novedad.ts
@@ -1,2 +1,15 @@
+import { Usuario } from './usuario';
+
 export class Novedad {
+    _id: string;
+    usuario: Usuario;
+    texto: string;
+    estado: string;
+
+    Novedad(_id?: string, usuario?: Usuario, texto?: string, estado?: string){
+        this._id = _id;
+        this.usuario = new Usuario();
+        this.texto = texto;
+        this.estado = estado;
+    }
 }
