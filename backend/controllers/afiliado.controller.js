7cebc4c62f17bcfbd8a37ec55c4c46f73ab8a016
getoconas <getoconas@gmail.com>
2020-06-26 00:27:14 -0300

    Archivos base afiliado (backend)

---
+++ backend/controllers/afiliado.controller.js
@@ -0,0 +1,15 @@
+const Afiliado = require('../models/afiliado');
+
+const afiliadoCtrl = {}
+
+// Listado de Afiliados
+
+// Alta de Afiliado
+
+// Obtener Afiliado por ID
+
+// Modificación de Afiliado
+
+// Baja de Afiliado
+
+module.exports = afiliadoCtrl;
---
+++ backend/routes/afiliado.routes.js
@@ -0,0 +1,12 @@
+// Creamos un manejador de rutas modulares
+const express = require('express');
+const router = express.Router();
+
+// Definimos el controlador para el manejo del CRUD
+/* afiliadoCtrl */
+
+// Definimos las rutas
+/* routes */
+
+// Exportamos el modulo de rutas
+module.exports = router;
