33e8848e9de7b113c36db1c187906a1fd5cfe043
getoconas <getoconas@gmail.com>
2020-06-26 22:27:51 -0300

    Alta de afiliados

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const { mongoose } = require('./database');
 const cors = require('cors');
 
 // Middlewares
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors({
   origin: 'http://localhost:4200'
 }))
--- frontend/src/app/components/afiliado/afiliado.component.ts
+++ frontend/src/app/components/afiliado/afiliado.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Afiliado } from 'src/app/models/afiliado';
+import { AfiliadoService } from 'src/app/service/afiliado.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-afiliado',
@@ -9,14 +11,45 @@ import { Afiliado } from 'src/app/models/afiliado';
 export class AfiliadoComponent implements OnInit {
 
   _afiliado: Afiliado;
+  _afiliados: Array<Afiliado>;
   _convertido: string;
 
-  constructor() { 
+  constructor(private _afiliadoService: AfiliadoService) { 
     this._afiliado = new Afiliado();
+    this._afiliados = new Array<Afiliado>();
+    this.obtenerAfiliados();
   }
 
-  public guardarAfiliado() {
+  public obtenerAfiliados() {
+    this._afiliadoService.getAfiliados().subscribe(
+      (result) => {
+        this._afiliados = new Array<Afiliado>();
+        result.forEach(element => {
+          var _afi: Afiliado = new Afiliado();
+          Object.assign(_afi, element);
+          this._afiliados.push(_afi);
+        })
+      }
+    )
+  }
 
+  public agregarAfiliado() {
+    this._afiliado.imagen = this._convertido;
+    console.log(this._afiliado);
+    this._afiliadoService.addAfiliado(this._afiliado).subscribe(
+      (result) => {
+        this.obtenerAfiliados();
+        alert('Afiliado Agregado');
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+    this.limpiarCampos();
+  }
+
+  public limpiarCampos() {
+    this._afiliado = new Afiliado();
   }
 
   public cambiarArchivo(file) {
