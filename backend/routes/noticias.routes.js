92f482fcd5474ea8793dc2543404c0ae56645b2e
yesicaMamani <yesica.mamani86@gmail.com>
2020-07-01 02:36:17 -0300

    se creo backend de noticias completo, probado

---
+++ backend/routes/noticias.routes.js
@@ -0,0 +1,16 @@
+// Creamos un manejador de rutas modulares
+const express = require('express');
+const router = express.Router();
+
+// Definimos el controlador para el manejo del CRUD
+const noticiaCtrl = require('./../controllers/noticia.controller');
+
+// Definimos las rutas
+router.get('/', noticiaCtrl.getNoticias);
+router.post('/', noticiaCtrl.createNoticia);
+router.get('/:id', noticiaCtrl.getNoticia);
+router.put('/:id', noticiaCtrl.editNoticia);
+router.delete('/:id', noticiaCtrl.deleteNoticia);
+
+// Exportamos el modulo de rutas
+module.exports = router;
