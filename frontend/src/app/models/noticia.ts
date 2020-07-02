386fb9202cfdab51c1550eb7e65410c4fb854243
yesicaMamani <yesica.mamani86@gmail.com>
2020-07-02 04:05:56 -0300

    se creo crud completo de noticias

--- frontend/src/app/models/noticia.ts
+++ frontend/src/app/models/noticia.ts
@@ -7,14 +7,16 @@ export class Noticia {
     fecha: Date;
     usuario: Usuario;
     vigente: boolean;
+    imagen: string;
 
-    Noticia(_id?: string, titulo?: string, descripcion?: string, fecha?: Date, usuario?: Usuario, vigente?: boolean){
+    Noticia(_id?: string, titulo?: string, descripcion?: string, fecha?: Date, usuario?: Usuario, vigente?: boolean, imagen?: string){
         this._id = _id;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.fecha = fecha;
         this.usuario = new Usuario();
         this.vigente = vigente;
+        this.imagen = imagen;
         
     }
 
