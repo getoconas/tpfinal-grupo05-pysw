586188aa98c79921ba6fb866008bae05e9e58ea5
LiliaSaid <yazminsaid@gmail.com>
2020-07-03 20:17:54 -0300

    backend model novedad

---
+++ backend/models/novedad.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Usuario = require('./usuario');
+const {Schema} = mongoose;
+
+const NoticiaSchema = new Schema({
+    usuario: {type: Schema.Types.ObjectId, ref: Usuario},
+    texto: {type: String, required: true},        
+    estado: {type: Boolean, required: true},    
+})
+
+module.exports = mongoose.model('Novedad', NovedadSchema);
